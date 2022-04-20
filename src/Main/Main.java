fd29308db389c608d5ca04cad338123815544aa0
Damien Cruz <dcruz99@uw.edu>
2022-04-19 18:14:52 -0700

    set up a starting point for GUI

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,13 @@
 package Main;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("holle world");
         System.out.println("Abdul change");
+        JFrame open = new View.mainGui();
+        open.setVisible(true);
     }
 
 }
--- src/View/mainGui.java
+++ src/View/mainGui.java
@@ -1,9 +1,32 @@
 package View;
 
-public class mainGui extends Jframe {
-    JWindow mainWindow = new JWindow;
-  public mainGui(){
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    this.add(mainWIndow);
-    this.setVisible(true);
+import javax.swing.*;
+import java.awt.*;
+
+public class mainGui extends JFrame {
+    private static final int SIZE_WIDTH = 900;//default starting width
+    private static final int SIZE_HEIGHT = 500;//default starting height
+    final JMenuItem exit = new JMenuItem("exit");//menu item for exiting
+
+    public mainGui() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);//so we can close the window with the windows exit button
+        setSize(SIZE_WIDTH, SIZE_HEIGHT);//setting the default size
+        var menuBar = new JMenuBar();//this is where menu items will live
+        setJMenuBar(menuBar);
+        JPanel mainPanel = new JPanel();// main container for everything to sit in
+        this.add(mainPanel);
+        mainPanel.setBackground(Color.white);//whatever  background color we want
+
+
+        menuBar.add(exit);//so you can exit the window from the menu bar
+
+
+
+
+        exit.addActionListener(
+                e -> System.exit(0)
+        );
+
+
+    }
 }
