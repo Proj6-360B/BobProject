20fa7652576baf4b5921293d9bffe71e2f0ea313
dav <XgarhontX@gmail.com>
2022-05-12 19:01:12 -0700

    Profile javadoc

--- src/Profile/Profile.java
+++ src/Profile/Profile.java
@@ -1,8 +1,21 @@
 package Profile;
 
+/**
+ * Profile. Stores a username, email, and the level of privilege.
+ * @author David Huynh
+ */
 public class Profile {
+    /**
+     * The username.
+     */
     private String myUsername;
+    /**
+     * The Email address.
+     */
     private String myEmail;
+    /**
+     * The privilege level.
+     */
     private Privilege myPrivilege;
 
     /**
@@ -17,30 +30,59 @@ public class Profile {
         setPrivilege(thePrivilege);
     }
 
+    /**
+     * Gets the username.
+     * @return the username
+     */
     public String getUsername() {
         return myUsername;
     }
 
+    /**
+     * Sets the username
+     * @param myUsername the username to set
+     */
     public void setUsername(String myUsername) {
         this.myUsername = myUsername;
     }
 
+    /**
+     * Get the privilege enum.
+     * @return the privilege enum
+     */
     public Privilege getPrivilege() {
         return myPrivilege;
     }
 
+    /**
+     * Set the privilege enum.
+     * @param myPrivilege the privilege enum to set
+     */
     public void setPrivilege(Privilege myPrivilege) {
         this.myPrivilege = myPrivilege;
     }
 
+    /**
+     * Get the email address.
+     * @return the email address
+     */
     public String getEmail() {
         return myEmail;
     }
 
+    /**
+     * Set the email address.
+     * @param myEmail the email address to set.
+     */
     public void setEmail(String myEmail) {
         this.myEmail = myEmail;
     }
 
+    /**
+     * toString(). <br>
+     * [myUsername] ([thePrivilege])
+     * @return
+     */
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
