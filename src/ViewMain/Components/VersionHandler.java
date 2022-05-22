d83ecf26fcb04f1339f8e1c89caa8089a00450f2
Dav Hunyah <xgarhontx@gmail.com>
2022-05-22 13:06:37 -0700

    Iteration2 (v0.2)

--- src/ViewMain/Components/VersionHandler.java
+++ src/ViewMain/Components/VersionHandler.java
@@ -1,5 +1,5 @@
 package ViewMain.Components;
 
 public class VersionHandler {
-    public static final String VERSION = "Iteration1 (v0.1)";
+    public static final String VERSION = "Iteration2 (v0.2)";
 }
